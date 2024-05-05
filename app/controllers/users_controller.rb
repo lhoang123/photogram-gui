5eade2a4c87c6fe73a0ec354d3ce2127ec38e8e1
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-05-05 21:11:12 +0000

    created show page for photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
   def show
   url_username = params.fetch("path_username")
 
-  the_user = User.where({ :username => url_username}).at(0)
+  @the_user = User.where({ :username => url_username}).at(0)
 
-    if the_user == nil
-      redirect_to ("/users")
+    if @the_user == nil
+      redirect_to ("/404")
     else
     render({ :template => "users_templates/show"})
     end
