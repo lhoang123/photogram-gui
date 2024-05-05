e52564969ae845ba4088ff33b2c588924e7e45fc
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-05-05 04:11:00 +0000

    created homepage

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,5 +1,8 @@
 class MiscController < ApplicationController
   def homepage
+    matching_users = User.all
+    @list_of_users = matching_users.order({ :username => :asc})
+
     render({ :template => "misc_templates/home"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/users", { :controller => "misc", :action => "homepage" })
+
+  get("/users/:username", { :controller => "misc", :action => "homepage" })
 end
